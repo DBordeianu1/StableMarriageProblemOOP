ba4dfd435bc05bbf1b25b2c21f54f61809e8e6f9
Daniela Bordeianu <danabordeianu55@gmail.com>
2026-01-31 16:38:06 -0500

    docs: add Javadoc documentation

--- Program.java
+++ Program.java
@@ -14,7 +14,13 @@ public class Program {
 	private Resident[] matchedResidents;
 	private int pointer; // points at the first null cell in matchedResidents, cannot be greater than quota
 	
-	// constructs a Program
+	/**
+	 * Constructs a Program
+	 * 
+	 * @param id the 3 letter identification for this program
+	 * @param n the name of the program
+	 * @param q the quota of the program, i.e. the available positions for it
+	 */
     public Program(String id, String n, int q) {
 		programID = id;
 		name = n;
@@ -23,28 +29,58 @@ public class Program {
 		pointer = 0;
 	}
 
-    // the rol in order of preference
+	/**
+	 * Sets the rol of the program in order of preference
+	 * 
+	 * @param rol the rol in order of preference
+	 */
 	public void setROL(int[] rol) {
 		this.rol= rol;
 	}
 	
-	// string representation
+	/**
+	 * Returns the string representation of the program
+	 * 
+	 * @return the string representation of the program
+	 */
 	public String toString() {
 		return "["+programID+"]: "+name+" {"+ quota+ "}" +" ("+rol.length+")";	  
 	}
 
-	public int avalible(){	// return number of avalible positions left
+	/**
+	 * Returns the number of available positions left
+	 * 
+	 * @return number of available positions left
+	 */
+	public int available(){
 		return (quota - pointer);
 	}
 
+	/**
+	 * Returns the 3 letter identification of a program
+	 * 
+	 * @return the 3 letter identification for this program
+	 */
 	public String getID(){
 		return programID;
 	}
 
+	/**
+	 * Returns the name of the program
+	 * 
+	 * @return the name of the program
+	 */
 	public String getName(){
 		return name;
 	}
 
+
+	/**
+	 * Returns true if the resident is a part of the rol of the program, false otherwise
+	 * 
+	 * @param residentID the identification of a resident
+	 * @return {@code true} if the resident is a part of the program rol, {@code false} otherwise
+	 */
 	public boolean member(int residentID){
 		for (int i=0;i<rol.length;i++){
 			if (rol[i] == residentID){
@@ -54,6 +90,13 @@ public class Program {
 		return false;
 	}
 
+
+	/**
+	 * Returns the rank of the resident, -1 if it is not present in the program's rol
+	 * 
+	 * @param residentID the identification of a resident
+	 * @return the resident's rank from the rol of the program
+	 */
 	public int rank(int residentID){
 		for (int i=0;i<rol.length;i++){
 			if (rol[i] == residentID){
@@ -63,8 +106,13 @@ public class Program {
 		return -1; //if resident not in the list
 	}
 
-	public Resident leastPreferred(){// returns a REFERENCE to leastPreferred resident in matchedResidents
-		int worstRank = 0; // highier rank = worse, starting worst rank = 0
+	/**
+	 * Returns a referencce of the program's least preferred resident
+	 * 
+	 * @return a reference of the least preferred resident in the list of matched residents
+	 */
+	public Resident leastPreferred(){
+		int worstRank = 0; // higher rank = worse, starting worst rank = 0
 		int worstResident = -1; // remembering worst resident
 		for (int i = (pointer - 1); i <= 0; i--){ // checking every matched resident 
 			if (matchedResidents[i].getRank() > worstRank){ // only if the residents rank is worse than current worst
@@ -75,7 +123,12 @@ public class Program {
 		return matchedResidents[worstResident];
 	}
 
-	public int leastPreferredPos(){ // returns the POSITION of leastPreferred resident in matchedResidents
+	/**
+	 * Returns the position of the program's least preferred resident
+	 * 
+	 * @return the position of the least preferred resident in the list of matched residents
+	 */
+	public int leastPreferredPos(){ 
 		int worstRank = 0; 
 		int worstResident = -1; 
 		for (int i = (pointer - 1); i >= 0; i--){ 
@@ -87,6 +140,12 @@ public class Program {
 		return worstResident;
 	}
 
+	/**
+	 * Returns the newly matched resident
+	 * 
+	 * @param resident a reference to the resident
+	 * @return a reference the resident added to the matched residents' list
+	 */
 	public Resident addResident(Resident resident){
 		int r = rank(resident.getID());
 		if (r != -1){ // chechking for member
